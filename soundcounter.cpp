5781ffac24033593827a35b5fd2283cfff0a2b1f
Florent Charpentier <florent.charpentier@epitech.eu>
2024-07-22 16:20:24 +0200

    :sparkles: Added special sound feecback each 50

--- soundcounter.cpp
+++ soundcounter.cpp
@@ -10,15 +10,22 @@ int main(void)
 {
 	unsigned counter = 0;
 	sf::Music beep;
+	sf::Music boop;
 	std::string s;
 
 	beep.openFromFile("assets/beep.wav");
+	boop.openFromFile("assets/boop.wav");
 	std::cout << "0" << std::endl;
 	while (std::getline(std::cin, s)) {
 		counter++;
 		std::cout << "\r" << counter << std::endl;
 		beep.stop();
-		beep.play();
+		boop.stop();
+		if (counter % 50) {
+			beep.play();
+		} else {
+			boop.play();
+		}
 		std::this_thread::sleep_for(30ms);
 	}
 	return 0;
